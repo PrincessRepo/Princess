f79a39f491b6875ea2423c43470f7557bb031fdf
Iker-kodi <32621419+Iker-kodi@users.noreply.github.com>
2018-08-25 20:56:57 +0200

    repo princess

--- Repositorio/resource.actualizador/uservar.py
+++ Repositorio/resource.actualizador/uservar.py
@@ -7,7 +7,7 @@
 import os, xbmc, xbmcaddon
 
 ADDON_ID       = xbmcaddon.Addon().getAddonInfo('id')
-ADDONTITLE     = '[B][COLOR aqua]Actualizador Auto Cleaner[/COLOR][/B]'
+ADDONTITLE     = '[B][COLOR blue]Actualizador Auto Cleaner[/COLOR][/B]'
 HOME           = xbmc.translatePath('special://home/')
 PLUGIN         = os.path.join(HOME,     'addons',    ADDON_ID)
 
