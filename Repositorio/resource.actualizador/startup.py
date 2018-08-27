13b4b74c2d4207b6af62a7015d2ea8d87e2a83f0
Iker-kodi <32621419+Iker-kodi@users.noreply.github.com>
2018-08-27 13:03:56 +0200

    repo princess

---
+++ Repositorio/resource.actualizador/startup.py
@@ -0,0 +1,41 @@
+######################################################################################################################################################
+##
+## STARTUP SERVICE
+##
+######################################################################################################################################################
+
+import xbmc, xbmcaddon, xbmcgui, xbmcplugin, os, sys, xbmcvfs, glob
+import shutil
+import urllib2,urllib
+import re
+import uservar
+from datetime import date, datetime, timedelta
+from resources.libs import wizard as wiz
+
+xbmc.executebuiltin("UpdateLocalAddons")
+xbmc.executebuiltin("UpdateAddonRepos")
+
+addon = xbmcaddon.Addon()
+addonname = addon.getAddonInfo('name')
+addonid = addon.getAddonInfo('id')
+dialog = xbmcgui.Dialog()
+runupdate = xbmc.getInfoLabel('Skin.String(Startup.Favourites.Path)')
+version = addon.getSetting('version')
+update = xbmc.executebuiltin('runupdate')
+if version < "1.3":	
+	xbmc.executebuiltin(runupdate)
+	
+ 
+__addon__ = xbmcaddon.Addon()
+__addonname__ = __addon__.getAddonInfo('name')
+__icon__ = __addon__.getAddonInfo('icon')
+ 
+line1 = "[COLOR blue]Wizard Princess Actualizado[/COLOR]"
+time = 20000 #in miliseconds
+ 
+xbmc.executebuiltin('Notification(%s, %s, %d, %s)'%(__addonname__,line1, time, __icon__))
+
+[COLOR blue]Actualizacion Wizard Princess v1.3 [/COLOR]
+
+
+
